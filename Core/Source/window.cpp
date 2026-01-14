9a76261cc2f039fd14f69e3acee0a8f3213b3b6e
jojo989 <70300454+jojo989@users.noreply.github.com>
2026-01-14 04:40:18 -0300

    fix cmakelists and removed inline from Window member functions

--- Core/Source/window.cpp
+++ Core/Source/window.cpp
@@ -6,12 +6,12 @@ namespace Core {
     Window::~Window() {
     }
 
-    inline void Window::create() {
+    void Window::create() {
         m_window.create(sf::VideoMode({ m_windowVars.width, m_windowVars.height }), m_windowVars.title, sf::Style::None);
                 // m_window.setFramerateLimit(m_frameRateLimit);
     }
 
-    inline void Window::update() {
+    void Window::update() {
         const std::optional<sf::Event> event = m_window.pollEvent();
         if (!event) { return; }
         if (event->is<sf::Event::Closed>()) {
@@ -19,7 +19,7 @@ namespace Core {
         }
     }
 
-    inline bool Window::shouldClose() const {
+    bool Window::shouldClose() const {
         return !m_window.isOpen();
     }
 }
